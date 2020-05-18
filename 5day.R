df47e616cb8338c545bf37e6cea32170677c77c4
sl984316 <sl985106@hanmail.net>
2020-05-18 11:40:33 +0900

    R 5day

--- 5day.R
+++ 5day.R
@@ -1,12 +1,49 @@
+#
+# Text file 읽기
+setwd("C:\\Workspace\\WorkspaceR")
+df <-  read.table(file="airquality.txt", header= T)
+df
+
+class(df)
 
 
 
+#
+# Excel file 읽기
+#
+# Excel 파일 읽기용 패키지 설치
+install.packages("xlsx")      # Excel 파일 읽기 패키지
+install.packages("rJava")     # Java 실행 패키지
+
+# 기본 패키지 외에 설치된 패키지 사용
+library(rJava)
+library(xlsx)
+
+df.xlsx <- read.xlsx(file="airquality.xlsx",
+                     sheetIndex = 1,
+                     encoding = "UTF-8")
+df.xlsx
+
+class(df.xlsx)
+str(df.xlsx)
+head(df.xlsx)
+tail(df.xlsx)
 
 
+score <- c(76,84,69,50, 95, 6, 85, 72,99, 84)
+which(score == 69)                    # 조건에 만족하는 위치의 index
+which(score >= 85)
+max (score)
+which.max(score)                      # 최고값의 index
+min(score)
+which.min(score)                      # 최저값의 index
 
+idx <- which(score >= 60)
+score[idx] <- 61
+score
 
-
-
+idx <- which(df.xlsx[,1:2]=="NA", arr.ind = TRUE)
+idx                                   # arr.ind = TRUE : 해당 조건의 행/열값을 확인할 때
 
 
 
