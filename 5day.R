3894300c6bbf49a74e82552c71b3793926294390
sl984316 <sl985106@hanmail.net>
2020-05-19 09:09:00 +0900

    5day file

--- 5day.R
+++ 5day.R
@@ -1,10 +1,14 @@
 #
 # Text file 읽기
 setwd("C:\\Workspace\\WorkspaceR")
-df <-  read.table(file="airquality.txt", header= T)
-df
+df.txt <-  read.table(file="airquality.txt", header= T,
+                      encoding = "UTF-8")
+df.txt
 
-class(df)
+class(df.txt)
+str(df.txt)
+head(df.txt)
+tail(df.txt)
 
 
 
@@ -46,17 +50,219 @@ idx <- which(df.xlsx[,1:2]=="NA", arr.ind = TRUE)
 idx                                   # arr.ind = TRUE : 해당 조건의 행/열값을 확인할 때
 
 
+#
+# 2.2 자료의 종류
+#
+# - 자료의 종류에 따라 적용할 수 있는 분석 방법이 다르다.
+# - 분석을 해야 할 자료를 가지고 있을 때 1차적으로 해야 할 일은 
+#   해당 자료가 어떤 분류에 속하는지를 파악하는 일이다.
+#
+#  자료 특성에 따른 분류
+#   - 분석 대상 자료의 특성에 따라 범주형 자료와 연속형 자료로 분류한다.
+#
+# 1. 범주형 자료 ( categorical data )
+#   - 범주 또는 그룹으로 구분할 수 있는 값
+#   - 범주형 자료의 값은 기본적으로 숫자로 표현할 수 없고,
+#     대소비교나 산술연산이 적용되지 않는다.
+#   - 범주형 자료는 대체로 문자형 값을 갖는데, 숫자로 표기할 수 있으나
+#     계산이 가능한 연속형 자료가 되는 것은 아니다.
+#
+# 2. 연속형 자료 ( numerical data )
+#   - 크기가 있는 숫자들로 구성된 자료
+#   - 연속형 자료의 값들은 대소비교, 산술연산이 가능하기 때문에 다양한 분석 방법이 존재한다.
+#
+#
+# 자료(data) : 어떤 주제를 가지고 값을 모아 놓은 것 전체
+# 관측값(observation) : 자료에 포함된 값들
+#
+# 통계학에서 변수는 "연구, 조사, 관찰하고 싶은 대상의 특성( feature )"
+#
+# 변수 개수에 따라
+#   1. 단일변수 자료 ( univariate data ), 일변량 자료
+#      하나의 변수로 구성된 자료
+#   2. 다중변수 자료 ( multivariate data ), 다변량 자료
+#      두개 이상의 변수로 구성된 자료
+#      특별히 두 개의 변수로 구성된 자료를 이변량 자료( bivariate data ) 라 한다.
+#
+#   단일 변수 자료는 vecter에 저장하여 분석
+#   다중 변수 자료는 matrix 또는 data frame에 저장하여 분석
+#
+#
+# 2.3 단일 변수 범주형 자료 탐색
+#
+# 단일 변수 범주형 자료 : 특성이 하나이면서 자료의 특성이 범주형인 자료
+#   범주형 자료에 할 수 있는 기본 작업 : 자료에 포함된 관측값들의 
+#                                        종류별로 개수를 세는 것(도수분포표),
+#                                        종류별로 비율을 구하는 것,
+#                                        시각화는 막대/원 그래프를 사용
+#
+# 
+favorite <- c('WINTER', 'SUMMER', 'SPRING', 'SUMMER', 'SUMMER', 
+              'FALL', 'FALL', 'SUMMER', 'SPRING', 'SPRING')
+favorite
+
+class(favorite)        # type
+str(favorite)          # 내용
+dim(favorite)          # data 수
+
+# 도수 분포표 작성
+table(favorite)
+
+# 도수 분포 비율
+table(favorite) / length(favorite) 
 
 
+# 시각화 (막대 그래프)
+ds <- table(favorite)
+ds
+
+class(ds)
+str(ds)
+dim(ds)
+
+barplot(ds, main = 'favorite season')
+
+ds.new <- ds[c(2,3,1,4)]
+ds.new
+
+barplot( ds.new, main = "favorite season")
+
+pie(ds, main = "favorite season")
+pie(ds.new, main = "favorite season")
 
 
+favorite.color <- c(2,3,2,1,1,2,2,
+                    1,3,2,1,3,2,1,2)
+ds <- table(favorite.color)
+ds
+
+barplot ( ds, main = "favorite season")
+
+colors <- c('yellow', 'red', 'blue')
+names(ds) <- colors;
+ds
+barplot(ds,main="favorite season",
+        col = colors)
+pie(ds, main="favorite season",
+    col = colors)
+
+#
+# 2.4 단일 변수 연속형 자료 탐색
+# 
+# 단일 변수 연속형 자료 : 특성이 하나이면서 자료의 특성이 연속형인 자료
+#           연속형 자료는 관측값들의 크기를 가지므로 다양한 분석방법 존재
+#
+# 1. 평균 (mean) : 하나의 값으로 전체를 대표할 수 있는 값,
+#                  이상값에 영향을 받는다.
+# 2. 중앙값 (median) : 자료의 값들을 크기순으로 정렬하였을 때, 가장 중앙에 위치한 값
+#                      이상값에 영향을 받지 않는다.
+# 3. 절사평균 (trimmed mean) : 자료의 관측값들 중에서 작은 값들의 하위 n% 와 큰 값의 상위 n%를 
+#                              제외하고 중간에 있는 나머지 값들만 가지고 평균을 계산하는 방식
+# 4. 사분위수 (quantile) : 주어진 자료에 있는 값들을 크기순으로 나열했을 때 4등분하는 지점에 있는 값
+#                          1사분위수(Q1), 2사분위수(Q2, 중앙값과 동일), 3사분위수(Q3),
+#                          전체 자료를 4개로 나누므로 4개 구간은 25%의 자료가 존재
+# 5. 산포 (distribution) : 주어진 자료에 있는 값들이 퍼져 있는 정도
+#     분산 (variance) : 주어진 자료의 각각의 값이 평균으로부터 떨어져 있는 정도를 계산하여 합산한 후
+#                       값들의 개수로 나누어 계산
+#     표준편차 (standard deviation) : 분산의 제곱근으로 계산
+#  
+#  
+#  시각화는 히스토그램(Histogram)과 상자 그래프(box plot)를 사용
+#
+weight <- c(60,62,64,65,68,69)
+weight
+
+weight.heavy <- c(weight, 120)
+weight.heavy
+
+# 평균
+mean(weight)
+mean(weight.heavy)
+
+# 중앙값
+median(weight)
+median(weight.heavy)
+
+# 절사 평균
+mean(weight, trim = 0.2)
+mean(weight.heavy, trim = 0.2)
+
+# 사분위수
+quantile(weight.heavy)
+quantile(weight.heavy, (0:10) / 10)
+summary(weight.heavy)
+
+# 산포
+#  - 분산
+var(weight)
+
+#  - 표준편차
+sd(weight)
+
+# 값의 범위
+range(weight)
+
+# 최대값과 최소값의 차이
+diff(range(weight))
+
+# 연속형 자료 시각화
+# Histogram : 연속형 자료의 분포를 시각화하는 도구
+#             연속형 자료에서의 구간을 나누고 구간에 속한
+#             값들의 개수를 세는 방법으로 사용
+class(cars)
+str(cars)
+cars
+
+dist <- cars[,2]
+dist
+
+boxplot.stats(dist)   # 구체적인 값을 파악할 때 사용
+                      # $stats : 사분위수, 최대 최소
+                      # $n : 관측값의 수
+                      # $conf : 중앙값에 대한 신뢰구간
+                      # $out : 특이값(이상치) 목록
+
+hist(dist, main = "Histogram for 제동거리",
+     xlab = "제동거리", ylab = "빈도수",
+     border="blue", col = "green",
+     las=2,breaks=5)
+
+# 상자 그래프 ( boxplot, 상자 수염 그래프 )
+#  - 사분위수를 그래프 형태로 시각화하는 도구
+#  - 상자그래프는 하나의 그래프로 데이터의 분포,
+#    형태를 포함한 다양한 정보를 전달
+#     - 자료의 전반적인 분포를 이해하는데 도움  
+#     - 구체적인 최소/최대/중앙값을 알기는 어렵다.
+boxplot(dist, main = "자동차 제동거리")
+
+boxplot.stats(dist)
+boxplot.stats(dist)$stats     # 정상범위 사분위수
+boxplot.stats(dist)$n         # 관측치 수
+boxplot.stats(dist)$conf      # 중앙값 신뢰구간
+boxplot.stats(dist)$out       # 특이값(이상치) 목록
+
+# 일변량 연속형 데이터 중 그룹으로 구성된 자료의 상자 그래프
+boxplot(Petal.Length~Species,data = iris,
+        main = "품종별 꽃받침 길이")
 
 
+iris
 
+boxplot(iris$Petal.Length~iris$Species,
+        main = "품종별 꽃받침 길이")
 
-
-
-
+# 한 화면에 여러 그래프 작성
+par(mfrow=c(1,3))      # 1x3 가상화면 분할
+barplot(table(mtcars$carb), main = "C",
+        xlab = "carburetors", ylab = "freq",
+        col="blue")
+barplot(table(mtcars$cyl), main = "Cyl",
+        xlab = "cyl", ylab = "freq",
+        col = "red")
+barplot(table(mtcars$gear), main = "g",
+        xlab = "gear", ylab =  "freq",
+        col = "yellow")
+par(mfrow=c(1,1))     # 가상화면 분할 해제
 
 
 
---
+++ YoonWanduk_20200518.R
@@ -0,0 +1,103 @@
+# 윤완덕 20200518
+
+# 문1
+
+# F F F M M F F F M M
+
+# 1.위의 자료를 gender 벡터에 저장하시오.
+gender <- c('F','F','F','M','M','F','F','F','M','M')
+gender
+
+# 2.gender에 있는 값들에 대해 도수분포표를 작성하여 출력하시오.
+table(gender)
+
+# 3.gender에 있는 값들에 대해 막대그래프를 작성하여 출력하시오.
+barplot(table(gender),main = '학급 성별')
+
+# 4. gender에 있는 값들에 대해 원그래프를 작성하여 출력하시오.
+pie(table(gender),main = '학급 성별')
+
+
+# 문2
+
+# 여름 겨울 봄 가을 여름 가을 겨울 여름 여름 가을
+
+# 1.위의 자료를 season 벡터에 저장하시오.
+season <- c('summer','winter','spring','fall','summer','fall','winter','summer','summer','fall')
+season
+
+# 2. season에 있는 값들에 대해 도수분포표를 작성하여 출력하시오.
+table(season)
+
+# 3.season에 있는 값들에 대해 막대그래프를 작성하여 출력하시오.
+barplot(table(season),main = '좋아하는 계절')
+
+# 4.season에 있는 값들에 대해 원그래프를 작성하여 출력하시오.
+pie(table(season),main = '좋아하는 계절')
+
+
+# 문3
+
+# KOR  ENG ATH HIST SOC MUSIC BIO EARTH PHY ART
+# 90 	85 	73 	80 	 85  65    78  50    68  96
+
+# 1. 위 데이터를 score 벡터에 저장하시오(과목명은 데이터 이름으로 저장)
+score <- c(90,85,73,80,85,65,78,50,68,96)
+names(score) <- c('KOR','ENG','ATH','HIST','SOC','MUSIC','BIO','EARTH','PHY','ART')
+
+# 2. score 벡터의 내용을 출력하시오.
+score
+
+# 3. 전체 성적의 평균과 중앙값을 각각 구하시오.
+mean(score)
+median(score)
+
+# 4. 전체 성적의 표준편차를 출력하시오.
+sd(score)
+
+# 5.가장 성적이 높은 과목의 이름을 출력하시오.
+names(score)[score==max(score)]
+
+# 6.성적에 대한 상자그림을 작성하고, 이상치에 해당하는 과목이 있으면 출력하시오.
+boxplot(score,main='학생 성적')  # 이상치 없음
+
+# 7.다음 조건을 만족하는 위 성적에 대한 히스토그램을 작성하시오.
+hist(score,main = '학생 성적', xlab='점수',ylab='과목 수',
+     border='white',col='purple',las=1,breaks=5)
+
+
+# 문4
+
+mtcars
+# 1. 중량(wt)의 평균값, 중앙값, 절사평균값(절사범위: 15%), 표준편차를 각각 구하시오.
+mean(mtcars$wt)
+median(mtcars$wt)
+mean(mtcars$wt, tim=0.15)
+sd(mtcars$wt)
+
+# 2. 중량(wt)에 대해 summary( ) 함수의 적용 결과를 출력하시오.
+summary(mtcars$wt)
+
+# 3.실린더수(cyl)에 대해 도수분포표를 출력하시오.
+table(mtcars$cyl)
+
+# 4.앞에서 구한 도수분포표를 막대그래프로 출력하시오.
+barplot(mtcars$cyl, main = '실린더 수')
+
+# 5. 중량(wt)의 히스토그램을 출력하시오.
+hist(mtcars$wt, main='중량 별 자동차 종류',xlab='중량',
+      ylab='자동차 종류',border='black',col='skyblue',las=1,breaks=7)
+
+# 6.중량(wt)에 대해 상자그림을 출력하시오.(단, 상자그림으로부터 관찰할 수 있는정보를 함께 출력하시오.)
+boxplot(mtcars$wt, main='weight')
+boxplot.stats(mtcars$wt)
+
+# 7. 배기량(disp)에 대한 상자그림을 출력하시오.(단, 상자그림으로부터 관찰할 수 있는 정보를 함께 출력하시오.)
+boxplot(mtcars$disp, main='배기량')
+boxplot.stats(mtcars$disp)
+
+# 8.기어수(gear)를 그룹 정보로 하여 연비(mpg) 자료에 대해 상자그림을 작성하고,
+#   각 그룹의 상자그림을 비교하여 관찰할 수 있는 것이 무엇인지 나타내시오.
+boxplot(mtcars$mpg~mtcars$gear, main='기어 수와 연비의 상관관계')
+
+
